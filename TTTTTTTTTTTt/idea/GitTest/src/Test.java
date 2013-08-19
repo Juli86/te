4049f8521eae8a11596e63f6b25228688d830040
Kovalenko Anton <Kovantonlenko@gmail.com>
2013-08-19 20:29:44 +0300

    commit

---
+++ TTTTTTTTTTTt/idea/GitTest/src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: hi
+ * Date: 18.08.13
+ * Time: 23:17
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
