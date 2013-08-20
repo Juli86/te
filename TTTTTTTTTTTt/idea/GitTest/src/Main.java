2c595759e3701a441b4e47842c833191cd4a153c
Juli86 <Vanil.ka@mail.com>
2013-08-20 17:22:00 +0300

    Update Main.java

--- TTTTTTTTTTTt/idea/GitTest/src/Main.java
+++ TTTTTTTTTTTt/idea/GitTest/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("hellfqsafsco from net");
+        System.out.println("AAA");
         System.out.println();
     }
 }
