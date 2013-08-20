9751f828089aee72bcbe94fe96d6b3bda260e4b9
Juli86 <Vanil.ka@mail.com>
2013-08-20 17:22:35 +0300

    Update Main.java

--- TTTTTTTTTTTt/idea/GitTest/src/Main.java
+++ TTTTTTTTTTTt/idea/GitTest/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("AAA");
-        System.out.println();
+        System.out.println("ACCAA");
+        System.out.println();ccc
     }
 }
